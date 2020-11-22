410286382ede4d2a58fb14e2f9cdf585b652e49e
l1oret <lloret.joseluis@gmail.com>
2020-11-22 10:40:37 +0100

    Fix result

--- ssg/js/1-0.js
+++ ssg/js/1-0.js
@@ -4,7 +4,7 @@ const socket = io('https://1-0.futbol/');
 socket.on('home', function (data) {
   const fixture = document.getElementById(data.id);
   const home = fixture.getElementsByClassName('score')[0];
-  const away = fixture.getElementsByClassName('score')[0];
+  const away = fixture.getElementsByClassName('score')[1];
 
   home.innerHTML = data.content.home;
   away.innerHTML = data.content.away;
